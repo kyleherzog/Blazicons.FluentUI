91e82075befd2df4c25ae243a0b1b360678ef25b
Kyle Herzog <holler@kyleherzog.com>
2022-12-29 16:43:13 -0600

    initial commit

---
+++ Blazicons.FluentUI.Generating/FluentUIGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+using Blazicons.Generating;
+using CodeCasing;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.Threading;
+
+namespace Blazicons.FluentUI.Generating;
+
+[Generator]
+internal class FluentUIGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        using var taskContext = new JoinableTaskContext();
+        var taskFactory = new JoinableTaskFactory(taskContext);
+        var downloader = new RepoDownloader(new Uri(Properties.Resources.DownloadAddress));
+        taskFactory.Run(
+            async () =>
+            {
+                await downloader.Download("assets\\/((?!Temp [LR]T[RL])[^\\/])*\\/SVG\\/\\w*\\.svg$").ConfigureAwait(true);
+            });
+
+        var svgFolder = Path.Combine(downloader.ExtractedFolder, $"{downloader.RepoName}-{downloader.BranchName}", "assets");
+        context.WriteIconsClass("FluentUiIcon", svgFolder, "*_24_regular.svg", GetPropertyName);
+
+        svgFolder = Path.Combine(downloader.ExtractedFolder, $"{downloader.RepoName}-{downloader.BranchName}", "assets");
+        context.WriteIconsClass("FluentUiFilledIcon", svgFolder, "*_24_filled.svg", GetPropertyName);
+
+        downloader.CleanUp();
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        // required by ISourceGenerator
+    }
+
+    private string GetPropertyName(string fileName)
+    {
+        const string prefix = "Ic_Fluent_";
+        var result = Path.GetFileNameWithoutExtension(fileName);
+        result = result.Substring(prefix.Length);
+        result = Regex.Replace(result, @"_24_\w*$", string.Empty);
+        return result.ToPascalCase();
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Blazicons.FluentUI
+Provides the Fluent UI icon library packaged as [Blazicons](https://github.com/kyleherzog/Blazicons), SVG icon components for Blazor.
+
+Check out the [Demo Site](http://blazicons.com).
+
+## Getting Started
+To get started using the Fluent UI Blazicons, just install the FluentUI.Bootstrap NuGet package.
+
+Next add the Blazicons reference to the `_Imports.razor` file in the Blazor project.
+
+```csharp
+@using Blazicons
+```
+
+Finally, add the Blazicon component to your Blazor pages/components.
+```html
+<Blazicon Svg="FluentUiIcon.Alert"></Blazicon>
+```
+
+## Parameters & Styling
+See the [Blazicons](https://github.com/kyleherzog/Blazicons) documentation for details on parameters and styling.
